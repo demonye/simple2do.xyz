9b4af0cb8e87ca32722552b4f7a44f49eeb46807
Eric Ye <eric.ye@facteon.global>
2020-12-12 20:01:39 +1300

    calling API to get and save todo list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,19 @@
 <template>
-  <v-content>
+  <div>
+    <v-snackbar v-model="snackbar" color="error" outlined :timeout="5000" top>
+      {{ errorMessage }}
+    </v-snackbar>
     <v-row align="center">
       <v-col
         sm="8" offset-sm="2"
         md="6" offset-md="3"
         lg="4" offset-lg="4"
       >
-        <v-card outlined>
+        <v-card outlined :loading="loading">
           <v-card-title>{{ title }}</v-card-title>
+          <v-btn block elevation="0" @click="reload">Reload</v-btn>
 
-          <v-form @submit.prevent="addTask">
+          <v-form @submit.prevent="add(newTask)">
             <v-container>
               <v-row>
                 <v-col cols="8" sm="9">
@@ -17,7 +21,7 @@
                 </v-col>
                 <v-col cols="4" sm="3">
                   <div class="mt-4" style="min-width: 76px">
-                    <v-btn x-small fab elevation="0" @click="addTask" color="teal" :disabled="!newTask">
+                    <v-btn x-small fab elevation="0" @click="add(newTask)" color="teal" :disabled="!newTask">
                       <v-icon color="white">mdi-check</v-icon>
                     </v-btn>
                     <v-btn x-small fab elevation="0" class="ml-2" @click="newTask=null" :disabled="!newTask">
@@ -37,7 +41,7 @@
                 <template v-for="item in todoList">
                   <v-list-item :key="item.id" v-if="!item.deleted">
                     <v-list-item-action>
-                      <v-checkbox v-model="item.done"></v-checkbox>
+                      <v-checkbox v-model="item.is_done" @change="toggle(item)"></v-checkbox>
                     </v-list-item-action>
 
                     <v-list-item-content>
@@ -45,7 +49,7 @@
                     </v-list-item-content>
 
                     <v-list-item-action>
-                      <v-btn small icon color="warning" @click="item.deleted = true">
+                      <v-btn small icon color="warning" @click="remove(item)">
                         <v-icon>mdi-trash-can-outline</v-icon>
                       </v-btn>
                     </v-list-item-action>
@@ -63,7 +67,7 @@
                 <template v-for="item in doneList">
                   <v-list-item :key="item.id" v-if="!item.deleted">
                     <v-list-item-action>
-                      <v-checkbox v-model="item.done"></v-checkbox>
+                      <v-checkbox v-model="item.is_done"></v-checkbox>
                     </v-list-item-action>
 
                     <v-list-item-content>
@@ -88,11 +92,12 @@
         </v-card>
       </v-col>
     </v-row>
-  </v-content>
+  </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import TaskService from '@/services/task'
+
 export default {
   name: 'TodoList',
   props: {
@@ -101,31 +106,77 @@ export default {
   data() {
     return {
       newTask: null,
-      allList: [
-        { id: 1, title: "sa devops assignment", done: false, deleted: false },
-        { id: 2, title: "keakaka tickets", done: false, deleted: false },
-        { id: 3, title: "infra provisioning", done: true, deleted: false },
-        { id: 4, title: "first round", done: true, deleted: false }
-      ]
+      allList: [],
+      loading: false,
+      snackbar: false,
+      errorMessage: null
     }
+  },
+  mounted() {
+    this.reload()
   },
   computed: {
     todoList() {
-      return this.allList.filter(item => !item.done)
+      return this.allList.filter(item => !item.is_done)
     },
     doneList() {
-      return this.allList.filter(item => !!item.done)
+      return this.allList.filter(item => !!item.is_done)
     }
   },
   methods: {
-    addTask() {
-      if (this.newTask) {
-        const newId = this.allList.length + 1
-        this.allList.push({
-          id: newId, title: this.newTask, done: false, deleted: false
+    reload() {
+      this.loading = true
+      this.errorMessage = null
+      TaskService.list()
+        .then(resp => this.allList = resp.data)
+        .catch(err => {
+          this.errorMessage = err
+          this.snackbar = true
         })
-        this.newTask = null
+        .then(() => this.loading = false)
+    },
+
+    add(task) {
+      if (task) {
+        const newId = this.allList.length + 1
+        this.loading = true
+        this.errorMessage = null
+        TaskService.create({title: task})
+          .then(resp => {
+            this.allList.push(resp.data)
+          })
+          .catch(err => {
+            this.errorMessage = err
+            this.snackbar = true
+          })
+          .then(() => {
+            this.newTask = null
+            this.reload()
+          })
       }
+    },
+
+    toggle(item) {
+      this.loading = true
+      this.errorMessage = null
+      TaskService.toggle(item.id, item.is_done)
+        .catch(err => {
+          this.errorMessage = err
+          this.snackbar = true
+        })
+        .then(() => this.reload())
+    },
+
+    remove(item) {
+      this.loading = true
+      this.errorMessage = null
+      TaskService.delete(item.id)
+        .then(resp => item.deleted = true)
+        .catch(err => {
+          this.errorMessage = err
+          this.snackbar = true
+        })
+        .then(() => this.reload())
     }
   }
 }
---
+++ src/services/task.js
@@ -0,0 +1,18 @@
+import { api } from '@/services/api'
+
+const rootPath = '/task/'
+
+export default {
+  list() {
+    return api().get(rootPath)
+  },
+  create(data) {
+    return api().post(rootPath, data)
+  },
+  toggle(pk, is_done) {
+    return api().patch(`${rootPath}/${pk}/`, {is_done: is_done})
+  },
+  delete(pk) {
+    return api().delete(`${rootPath}/${pk}/`)
+  }
+}
